de3be1e6a0fa25095b72dc77585c288724b36972
Esme Y <61486616+esyeng@users.noreply.github.com>
2023-11-25 00:10:44 -0500

    Adds API url

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import luvnotesicon from './assets/luvnotesicon.svg'
 import { HeaderStyles, Image, RowContainer, StyledButton, Title } from '../styled-components'
+import { API_URL } from '../constants'
 
 const Header: React.FC = () => {
     return (
---
+++ client/src/constants.ts
@@ -0,0 +1,1 @@
+export const API_URL = 'https://morning-woodland-11303-10a408c87697.herokuapp.com/';
